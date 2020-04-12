6b27af17cd3a77a88b119c10abdf57b5ecf55760
brianbellini <brianbellini@me.com>
2020-04-12 15:19:13 -0600

    Router setup

---
+++ config/database.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+});
+
+const db = mongoose.connection;
+
+db.on('connected', () => {
+  console.log(`Connected to MongoDB\nNAME:${db.name}\nHOST:${db.host}\nPORT:${db.port}`);
+});
