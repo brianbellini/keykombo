251caba0edfc1912551b66063a0c0466a3f43712
brianbellini <brianbellini@me.com>
2020-04-12 17:38:11 -0600

    Password hashing

--- models/user.js
+++ models/user.js
@@ -1,24 +1,43 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
+const SALT_ROUNDS = 6;
 
 const userSchema = new Schema({
-  name: {
-      type: String,
-      required: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  email: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true
-  }
-}, {
-  timestamps: true
+    name: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    }
+    }, {
+    timestamps: true
+});
+
+userSchema.set('toJSON', {
+    transform: function(doc, ret) {
+        delete ret.password;
+        return ret;
+    }
+});
+
+userSchema.pre('save', function(next) {
+    const user = this;
+    if (!user.isModified('password')) return next();
+    bcrypt.hash(user.password, SALT_ROUNDS, function(err, hash) {
+        if (err) return(err);
+        user.password = hash;
+        next();
+    });
 });
 
 module.exports = mongoose.model('User', userSchema);
