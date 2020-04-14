5ff44bff4e18f5b2c321795fb7f71d08071b8990
brianbellini <brianbellini@me.com>
2020-04-14 11:17:15 -0600

    Shortcut edit page form

---
+++ src/pages/ShortcutEditPage/ShortcutEditPage.jsx
@@ -0,0 +1,86 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+class ShortcutEditPage extends Component {
+  state = {
+    invalidForm: false,
+    formData: this.props.location.state.shortcut
+  };
+
+  formRef = React.createRef();
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.handleUpdateShortcut(this.state.formData);
+  };
+
+  handleChange = e => {
+    this.setState({
+      formData: {
+        ...this.state.formData,
+        [e.target.name]: e.target.value
+      },
+      invalidForm: !this.formRef.current.checkValidity()
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <h1>Edit Shortcut</h1>
+        <form ref={this.formRef} autoComplete="off" onSubmit={this.handleSubmit}>
+        <div>
+          <div>
+            <label>Application</label>
+            <input
+              name="application"
+              value={this.state.formData.application}
+              onChange={this.handleChange}
+              required/>
+          </div>
+            <label>Function</label>
+            <input
+              name="function"
+              value={this.state.formData.function}
+              onChange={this.handleChange}
+              required/>
+          </div>
+          <div>
+            <label>Description</label>
+            <input
+              name="description"
+              value={this.state.formData.description}
+              onChange={this.handleChange}
+              required/>
+          </div>
+          <div>
+            <label>Keys</label>
+            <input
+              name="combo"
+              value={this.state.formData.combo}
+              onChange={this.handleChange}
+              required/>
+          </div>
+          <div>
+            <label>Menu</label>
+            <input
+              name="menu"
+              value={this.state.formData.menu}
+              onChange={this.handleChange}
+              required/>
+          </div>
+          <button
+            type="submit"
+            className="btn"
+            disabled={this.state.invalidForm}
+          >
+            SAVE CHANGES
+          </button>&nbsp;&nbsp;
+          <Link to='/'>CANCEL</Link>
+        </form>
+      </>
+    );
+  }
+}
+
+export default EditPuppyPage;
